d7a969c0a56bda83be6f1126af2221c8ca902a2e
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-12-22 19:16:13 +0900

    Added insert data sql.

---
+++ sql/insert.sql
@@ -0,0 +1,44 @@
+use eword;
+/*insert into user (mail) values  ('stevenlabs@gmail.com');*/
+insert into word (user_id, word, definition, priority) values (1, 'elaborate',
+ 'elaborate
+     adj 1: marked by complexity and richness of detail; "an elaborate
+            lace pattern" [syn: {luxuriant}]
+     2: developed or executed with care and in minute detail; "a
+        detailed plan"; "the elaborate register of the inhabitants
+        prevented tax evasion"- John Buchan; "the carefully
+        elaborated theme" [syn: {detailed}, {elaborated}]
+     v 1: add details, as to an account or idea; clarify the meaning
+          of and discourse in a learned way, usually in writing;
+          "She elaborated on the main ideas in her dissertation"
+          [syn: {lucubrate}, {expatiate}, {exposit}, {enlarge}, {flesh
+          out}, {expand}, {expound}, {dilate}] [ant: {abridge}]
+     2: produce from basic elements or sources; change into a more
+        developed product; "The bee elaborates honey"
+     3: make more complex, intricate, or richer; "refine a design or
+        pattern" [syn: {complicate}, {refine}, {rarify}]
+     4: work out in detail; "elaborate a plan" [syn: {work out}]
+', 1);
+
+insert into word (user_id, word, definition, priority) values (1, 'attend',
+ 'attend
+     v 1: be present at (meetings, church services, university), etc.;
+          "She attends class regularly"; "I rarely attend services
+          at my church"; "did you go to the meeting?" [syn: {go to}]
+          [ant: {miss}]
+     2: take charge of or deal with; "Could you see about lunch?";
+        "I must attend to this matter"; "She took care of this
+        business" [syn: {take care}, {look}, {see}]
+     3: to accompany as a circumstance or follow as a result;
+        "Menuhins playing was attended by a 15-minute standing
+        ovation"
+     4: work for or be a servant to; "May I serve you?"; "She
+        attends the old lady in the wheelchair"; "Can you wait on
+        our table, please?"; "Is a salesperson assisting you?";
+        "The minister served the King for many years" [syn: {serve},
+         {attend to}, {wait on}, {assist}]
+     5: give heed (to); "The children in the audience attended the
+        recital quietly"; "She hung on his every word"; "They
+        attended to everything he said" [syn: {hang}, {advert}, {pay
+        heed}, {give ear}]
+', 2);
