cca27137b6bda91dafa2e398ded2e6a14d3e2567
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-12-24 03:27:06 +0900

    Changed dictionary.

--- lib/EWord/Web/C/EWord.pm
+++ lib/EWord/Web/C/EWord.pm
@@ -2,7 +2,7 @@ package EWord::Web::C::EWord;
 use strict;
 use warnings;
 use Data::Dumper;
-use EWord::M::DictScraper2;
+use EWord::M::DictAPI;
 
 sub list {
     my ($class, $c) = @_;
@@ -12,7 +12,9 @@ sub list {
     while (my $row = $itr->next) {
         my $id   = $row->get_column('id');
         my $word = $row->get_column('word');
-        push @{$data}, {$id => $word};
+        my $def = $row->get_column('definition');
+        my $priority = $row->get_column('priority');
+        push @{$data}, {id => $id, word => $word, definition => $def, priority => $priority};
     }
 
     return $c->render_json($data);
@@ -27,7 +29,7 @@ sub definition {
 
     my $itr = $c->db->search("word", {}, {order_by => {'priority' => 'desc'}});
     $row->update({priority => $itr->first->get_column('priority') + 1});
-    my $data = {$row->get_column('id') => $row->get_column('definition')};
+    my $data = {def => $row->get_column('definition')};
     return $c->render_json($data);
 }
 
@@ -35,16 +37,8 @@ sub input {
     my ($class, $c) = @_;
 
     my $word = $c->request->param("word");
-    my $defines = EWord::M::DictScraper2->scrape($word);
-    return $c->render_json({result => "error"}) unless $defines->{body};
-
-    my $definition;
-    foreach my $key (keys %{$defines}) {
-        while (my $line = shift @{$defines->{$key}}) {
-            next unless defined $line;
-            $definition .= $line . "\n";
-        }
-    }
+    my $definition = EWord::M::DictAPI->get_def("wn", $word);
+    return $c->render_json({result => "error"}) unless $definition;
 
     my $row = $c->db->find_or_create("word",
             {
