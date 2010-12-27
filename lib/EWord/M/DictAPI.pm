e5b9c895bd9ec0bed48c832bba29568239549c72
Kazuhiro Shibuya <stevenlabs@gmail.com>
2010-12-27 19:16:47 +0900

    Added formatting.

--- lib/EWord/M/DictAPI.pm
+++ lib/EWord/M/DictAPI.pm
@@ -18,10 +18,21 @@ sub get_def {
 
     my $res = $ua->get($uri);
     if ($res->is_success) {
-        warn $res->content;
         my $dom = XML::LibXML->load_xml(string => $res->content);
         my ($top, $node) = $dom->getElementsByTagName("WordDefinition");
-        return $node->textContent;
+        my $node_txt = $node->textContent;
+        $node_txt =~ s/(\r|\n)//g;
+        $node_txt =~ s/\s{2,}/ /g;
+        $node_txt =~ s/;/;\n/g;
+        #my $text;
+        #my $offset = 0;
+        #my $length = 20;
+        #while ($offset < length $node_txt) {
+        #    my $line = substr $node_txt, $offset, $length;
+        #    $text .= $line . "\n";
+        #    $offset += $length;
+        #}
+        return $node_txt;
     }
     else {
         return undef;
